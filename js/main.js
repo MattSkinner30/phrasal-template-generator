86331011219cef375435872431da1d4c9f6a49b0
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-03-01 13:59:13 -0500

    Fixed errors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert('Let’s make a story together!');
+var intro = alert('Let’s make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
@@ -10,17 +10,13 @@ var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? \na) Snacking \nb) Escaping \nc) Winning \nEnter the letter below:');
 
-
 if (story == 'a') {
-	document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
-} 
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+}
 
 if (story == 'b') {
-	document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space creature.');
-} 
-
+  document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space creature.');
+}
 if (story == 'c') {
-	document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
-} 
-
-
+  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+}
