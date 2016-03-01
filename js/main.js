891f05ff1c0d17b38273d76c48320fe93f7cc8a7
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-03-01 12:34:43 -0500

    Commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+alert('Let’s make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = 'a';
+
+if (story == 'a') {
+	document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+} 
+
+if (story == 'b') {
+	document.write('Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.');
+} 
+
+if (story == 'c') {
+	document.write('Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.');
+} 
+
+
